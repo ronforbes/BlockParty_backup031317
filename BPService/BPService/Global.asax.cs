aff182ca0fc4bcf038a2f36482861103638277c2
Ron Forbes <ronforbes528@gmail.com>
2015-07-06 08:20:48 -0700

    Revert "Revert "Facebook and Block Party service login""
    This reverts commit bfccafc8e2267be2341bbe6c9b598ac701009844.

---
+++ BPService/BPService/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace BPService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        GameClock clock;
+
+        protected void Application_Start()
+        {
+            WebApiConfig.Register();
+
+            clock = new GameClock();
+        }
+    }
+}
