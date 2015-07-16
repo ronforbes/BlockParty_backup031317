db610aa200a8aff2e8842f34c5297b440ada697f
Ron Forbes <ronforbes528@gmail.com>
2015-07-16 08:38:43 -0700

    UI improvements

--- BPClient/Assets/Scripts/Results/LeaderboardReceiver.cs
+++ BPClient/Assets/Scripts/Results/LeaderboardReceiver.cs
@@ -4,53 +4,54 @@ using Bitrave.Azure;
 using System.Collections.Generic;
 using System;
 
-public class LeaderboardReceiver : MonoBehaviour {
-    bool requestedLeaderboard = false;
+public class LeaderboardReceiver : MonoBehaviour
+{
+	bool requestedLeaderboard = false;
 	
 	// Update is called once per frame
-	void Update () {
-	    if(GameClock.Instance.TimeRemaining.Seconds <= 5 && !requestedLeaderboard)
-        {
-            WWW leaderboardHttpRequest = new WWW("http://blockparty.azure-mobile.net/api/Leaderboard");
-            //WWW leaderboardHttpRequest = new WWW("http://localhost:49753/api/Leaderboard");
-            StartCoroutine(OnLeaderboardHttpRequest(leaderboardHttpRequest));
+	void Update ()
+	{
+		if (GameClock.Instance.TimeRemaining.Seconds <= 5 && !requestedLeaderboard) {
+			WWW leaderboardHttpRequest = new WWW ("http://blockparty.azure-mobile.net/api/Leaderboard");
+			//WWW leaderboardHttpRequest = new WWW("http://localhost:49753/api/Leaderboard");
+			StartCoroutine (OnLeaderboardHttpRequest (leaderboardHttpRequest));
             
-            requestedLeaderboard = true;
-        }
+			requestedLeaderboard = true;
+		}
 	}
 
-    IEnumerator OnLeaderboardHttpRequest(WWW httpRequest)
-    {
-        // Wait until the HTTP request has received a response
-        yield return httpRequest;
+	IEnumerator OnLeaderboardHttpRequest (WWW httpRequest)
+	{
+		// Wait until the HTTP request has received a response
+		yield return httpRequest;
 
-        // Deserialize the response into a list of objects
-        List<object> list = Facebook.MiniJSON.Json.Deserialize(httpRequest.text) as List<object>;
+		// Deserialize the response into a list of objects
+		List<object> list = Facebook.MiniJSON.Json.Deserialize (httpRequest.text) as List<object>;
 
-        int rank = 1;
+		int rank = 1;
 
-        foreach(object dict in list)
-        {
-            Dictionary<string, object> d = dict as Dictionary<string, object>;
+		foreach (object dict in list) {
+			Dictionary<string, object> d = dict as Dictionary<string, object>;
 
-            LeaderboardEntry entry = new LeaderboardEntry();
+			LeaderboardEntry entry = new LeaderboardEntry ();
 
-            entry.Rank = rank;
-            entry.Name = d["userName"] as string;           
+			entry.Rank = rank;
+			entry.Name = d ["userName"] as string;
+
+			if (entry.Name == "") {
+				entry.Name = "Guest";
+			}
             
-            try
-            {
-                entry.Score = int.Parse(d["score"].ToString());
-            }
-            catch(Exception e)
-            {
-                e = null;
-                entry.Score = 0;
-            }
+			try {
+				entry.Score = int.Parse (d ["score"].ToString ());
+			} catch (Exception e) {
+				e = null;
+				entry.Score = 0;
+			}
 
-            Leaderboard.Instance.LeaderboardEntries.Add(entry);
+			Leaderboard.Instance.LeaderboardEntries.Add (entry);
 
-            rank++;
-        }
-    }
+			rank++;
+		}
+	}
 }
--- BPClient/Assets/Scripts/Shared/UserManager.cs
+++ BPClient/Assets/Scripts/Shared/UserManager.cs
@@ -5,99 +5,90 @@ using System.Collections.Generic;
 
 public class UserManager : MonoBehaviour
 {
-    static UserManager instance;
-    public static UserManager Instance
-    {
-        get
-        {
-            // Get the singleton instance of the Game Clock
-            if (instance == null)
-            {
-                instance = GameObject.FindObjectOfType<UserManager>();
+	static UserManager instance;
+	public static UserManager Instance {
+		get {
+			// Get the singleton instance of the Game Clock
+			if (instance == null) {
+				instance = GameObject.FindObjectOfType<UserManager> ();
 
-                DontDestroyOnLoad(instance.gameObject);
-            }
+				DontDestroyOnLoad (instance.gameObject);
+			}
 
-            return instance;
-        }
-    }
+			return instance;
+		}
+	}
 
-    public string Id;
-    public string Name;
-    public Texture2D Picture;
+	public string Id;
+	public string Name;
+	public Texture2D Picture;
 
-    void Awake()
-    {
-        // If this is the first instance, make it the singleton. If a singleton already exists and another reference is found, destroy it
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this);
-        }
-        else
-        {
-            if (this != instance)
-                Destroy(this.gameObject);
-        }
-    }
+	void Awake ()
+	{
+		// If this is the first instance, make it the singleton. If a singleton already exists and another reference is found, destroy it
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this);
+		} else {
+			if (this != instance)
+				Destroy (this.gameObject);
+		}
+	}
 
-    // Use this for initialization
-    void Start ()
-    {
-        // Initialize the Facebook SDK
-        FB.Init(OnInitialized);
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		// Initialize the Facebook SDK
+		FB.Init (OnInitialized);
+	}
 
-    void OnInitialized()
-    {
+	void OnInitialized ()
+	{
 
-    }
+	}
 
-    public void Login()
-    {
-        // Log into Facebook
-        FB.Login("", OnFacebookLoggedIn);
-    }
+	public void Login ()
+	{
+		// Log into Facebook
+		FB.Login ("", OnFacebookLoggedIn);
+	}
 
-    void OnFacebookLoggedIn(FBResult result)
-    { 
-        // Log into the Block Party service
-        NetworkingManager.Instance.Service.LoginAsync(AuthenticationProvider.Facebook, FB.AccessToken, OnServiceLoggedIn);
+	void OnFacebookLoggedIn (FBResult result)
+	{ 
+		// Log into the Block Party service
+		NetworkingManager.Instance.Service.LoginAsync (AuthenticationProvider.Facebook, FB.AccessToken, OnServiceLoggedIn);
 
-        // Request the Facebook user profile
-        FB.API("/me", Facebook.HttpMethod.GET, OnGetMe);
+		// Request the Facebook user profile
+		FB.API ("/me", Facebook.HttpMethod.GET, OnGetMe);
 
-        // Request the Facebook profile picture
-        StartCoroutine("LoadProfilePicture");
-    }
+		// Request the Facebook profile picture
+		StartCoroutine ("LoadProfilePicture");
+	}
 
-    void OnServiceLoggedIn(AzureResponse<MobileServiceUser> response)
-    {
-        // If the login attempt was successful, save the user object. Otherwise, log the error
-        if (response.Status == AzureResponseStatus.Success)
-        {
-            NetworkingManager.Instance.Service.User = response.ResponseData;
-        }
-        else
-        {
-            Debug.Log("Error:" + response.StatusCode);
-        }
-    }
+	void OnServiceLoggedIn (AzureResponse<MobileServiceUser> response)
+	{
+		// If the login attempt was successful, save the user object. Otherwise, log the error
+		if (response.Status == AzureResponseStatus.Success) {
+			NetworkingManager.Instance.Service.User = response.ResponseData;
+		} else {
+			Debug.Log ("Error:" + response.StatusCode);
+		}
+	}
 
-    void OnGetMe(FBResult result)
-    {
-        // Deserialize the result, and save the name
-        Dictionary<string, object> profile = Facebook.MiniJSON.Json.Deserialize(result.Text) as Dictionary<string, object>;
-        Id = profile["id"] as string;
-        Name = profile["name"] as string;
-    }
+	void OnGetMe (FBResult result)
+	{
+		// Deserialize the result, and save the name
+		Dictionary<string, object> profile = Facebook.MiniJSON.Json.Deserialize (result.Text) as Dictionary<string, object>;
+		Id = profile ["id"] as string;
+		Name = profile ["name"] as string;
+	}
 
-    IEnumerator LoadProfilePicture()
-    {
-        // Wait for the HTTP request's response, and store the picture
-        WWW httpRequest = new WWW("https://graph.facebook.com/me/picture?access_token=" + FB.AccessToken);
-        yield return httpRequest;
-        Picture = new Texture2D(128, 128, TextureFormat.DXT1, true);
-        httpRequest.LoadImageIntoTexture(Picture);
-    }
+	IEnumerator LoadProfilePicture ()
+	{
+		// Wait for the HTTP request's response, and store the picture
+		WWW httpRequest = new WWW ("https://graph.facebook.com/me/picture?access_token=" + FB.AccessToken);
+		yield return httpRequest;
+		Picture = new Texture2D (128, 128, TextureFormat.RGB24, true);
+		httpRequest.LoadImageIntoTexture (Picture);
+	}
 }
