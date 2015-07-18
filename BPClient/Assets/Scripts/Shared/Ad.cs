58b72bcb1813fa2a0bff3d9736e41338aeaab913
Ron Forbes <ronforbes528@gmail.com>
2015-07-18 15:28:53 -0700

    Added ad integration

---
+++ BPClient/Assets/Scripts/Shared/Ad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ad : MonoBehaviour
+{
+	UnityEngine.iOS.ADBannerView ad;
+
+	// Use this for initialization
+	void Start ()
+	{
+		ad = new UnityEngine.iOS.ADBannerView (UnityEngine.iOS.ADBannerView.Type.Banner, UnityEngine.iOS.ADBannerView.Layout.Bottom);
+		UnityEngine.iOS.ADBannerView.onBannerWasClicked += OnBannerWasClicked;
+		UnityEngine.iOS.ADBannerView.onBannerWasLoaded += OnBannerWasLoaded;
+	}
+
+	void OnBannerWasClicked ()
+	{
+
+	}
+
+	void OnBannerWasLoaded ()
+	{
+		ad.visible = true;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
