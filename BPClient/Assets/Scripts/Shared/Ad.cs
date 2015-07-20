253c36ce613673ca4edba316d5eb115561900ca1
Ron Forbes <ronforbes528@gmail.com>
2015-07-19 19:52:41 -0700

    Fixed ads to show on results and lobby

--- BPClient/Assets/Scripts/Shared/Ad.cs
+++ BPClient/Assets/Scripts/Shared/Ad.cs
@@ -3,11 +3,39 @@ using System.Collections;
 
 public class Ad : MonoBehaviour
 {
+	static Ad instance;
+	public static Ad Instance {
+		get {
+			// Get the singleton instance of the Game Clock
+			if (instance == null) {
+				instance = GameObject.FindObjectOfType<Ad> ();
+				
+				DontDestroyOnLoad (instance.gameObject);
+			}
+			
+			return instance;
+		}
+	}
+
 	UnityEngine.iOS.ADBannerView ad;
+
+	void Awake ()
+	{
+		// If this is the first instance, make it the singleton. If a singleton already exists and another reference is found, destroy it
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this);
+		} else {
+			if (this != instance)
+				Destroy (this.gameObject);
+		}
+	}
 
 	// Use this for initialization
 	void Start ()
 	{
+		DontDestroyOnLoad (gameObject);
+
 		ad = new UnityEngine.iOS.ADBannerView (UnityEngine.iOS.ADBannerView.Type.Banner, UnityEngine.iOS.ADBannerView.Layout.Bottom);
 		UnityEngine.iOS.ADBannerView.onBannerWasClicked += OnBannerWasClicked;
 		UnityEngine.iOS.ADBannerView.onBannerWasLoaded += OnBannerWasLoaded;
@@ -26,6 +54,21 @@ public class Ad : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if (!ad.visible) {
+			if (Application.loadedLevelName == "Results" || Application.loadedLevelName == "Lobby") {
+				ad.visible = true;
+			}
+		}
+
+		if (ad.visible) {
+			if (Application.loadedLevelName == "Menu" || Application.loadedLevelName == "Game") {
+				ad.visible = false;
+			}
+		}
+	}
+
+	void OnDestroy ()
+	{
+		ad.visible = false;
 	}
 }
