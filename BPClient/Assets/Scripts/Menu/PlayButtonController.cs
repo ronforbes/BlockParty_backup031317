7c1943e8d906b9136a911b80a3157a9bffb6a260
Ron Forbes <ronforbes528@gmail.com>
2015-08-16 17:16:34 -0700

    Backing up art and certs

--- BPClient/Assets/Scripts/Menu/PlayButtonController.cs
+++ BPClient/Assets/Scripts/Menu/PlayButtonController.cs
@@ -1,11 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayButtonController : MonoBehaviour
 {
-    public void Play()
-    {
-        string level = GameClock.Instance.State.ToString();
-        Application.LoadLevel(level);
-    }	
+	Button playButton;
+	Text playButtonText;
+
+	void Start ()
+	{
+		playButton = GetComponent<Button> ();
+		playButtonText = GameObject.Find ("Play Button Text").GetComponent<Text> ();
+	}
+
+	public void Play ()
+	{
+		string level = GameClock.Instance.State.ToString ();
+		Application.LoadLevel (level);
+	}	
+
+	void Update ()
+	{
+		if (GameClock.Instance.State == GameClock.ClockState.Default) {
+			playButton.interactable = false;
+			playButtonText.text = "Loading...";
+		} else {
+			playButton.interactable = true;
+			playButtonText.text = "Play";
+		}
+	}
 }
