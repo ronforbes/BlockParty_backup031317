cc4ef653019bf7758ae6c052e1109f9892e56a7b
Ron Forbes <ronforbes528@gmail.com>
2015-08-21 22:50:35 -0400

    Animated Game Scene
    Made more progress on animating the UI. A side effect of the hack
    approach I’m using is that the generic Back Button Controller throws an
    error on the other scenes because it can’t find the Game scene-specific
    UI elements. Need to find a better way…

--- BPClient/Assets/Scripts/Shared/BackButtonController.cs
+++ BPClient/Assets/Scripts/Shared/BackButtonController.cs
@@ -3,8 +3,27 @@ using System.Collections;
 
 public class BackButtonController : MonoBehaviour
 {
+	Animator gameUIAnimator;
+	Animator boardPanelAnimator;
+
+	void Start()
+	{
+		gameUIAnimator = GameObject.Find ("Game UI").GetComponent<Animator> ();
+		boardPanelAnimator = GameObject.Find ("Board Panel").GetComponent<Animator> ();
+	}
+
     public void Back()
     {
-        Application.LoadLevel("Menu");
-    }
+		gameUIAnimator.SetBool ("isHidden", true);
+		boardPanelAnimator.SetBool ("isHidden", true);
+
+		StartCoroutine (WaitAndLoadLevel());
+	}	
+	
+	IEnumerator WaitAndLoadLevel()
+	{
+		yield return new WaitForSeconds (1.0f);
+
+		Application.LoadLevel("Menu");
+	}
 }
